e7a78ff0b86a705ec6dfd8928f92e8e5f8be52ca
Equan P <equan.p@gmail.com>
2023-03-16 14:35:43 +0700

    fetchCountryPopulationData()

--- packages/data-server/libs/worldPopulationData.js
+++ packages/data-server/libs/worldPopulationData.js
@@ -2,8 +2,13 @@ import puppeteer from "puppeteer";
 
 const retrieveDataInterval = 3;
 const worldPopDataSource = "https://www.worldometers.info/world-population/";
+const worldPopDataByCountrySource =
+  "https://www.worldometers.info/world-population/population-by-country/";
 
-const fetchData = async () => {
+/**
+ * fetch world population data
+ */
+const fetchWorldPopulationData = async () => {
   // Launch a new browser instance
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -27,4 +32,39 @@ const fetchData = async () => {
   extractData();
 };
 
-export { fetchData as fetchWorldPopulationData };
+/**
+ * fetch world population data by country
+ */
+const fetchCountryPopulationData = async () => {
+  // Launch a new browser instance
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate to the target website
+  await page.goto(worldPopDataByCountrySource);
+
+  // Extract data from the page
+  const data = await page.evaluate(() => {
+    const tableRows = document.querySelectorAll("#example2 tbody tr");
+    const countryPopulationData = [];
+
+    tableRows.forEach((row) => {
+      const countryElement = row.querySelector("td:nth-child(2) a");
+      const country = countryElement.textContent.trim();
+
+      const populationElement = row.querySelector("td:nth-child(3)");
+      const population = populationElement.textContent.trim();
+
+      countryPopulationData.push({ country, population });
+    });
+
+    return countryPopulationData;
+  });
+
+  // Close the browser
+  await browser.close();
+
+  return data;
+};
+
+export { fetchCountryPopulationData, fetchWorldPopulationData };
