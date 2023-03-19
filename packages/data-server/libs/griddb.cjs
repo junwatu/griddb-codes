78d5e3f79f83820eb2026700dd5846470e11cf4b
Equan P <equan.p@gmail.com>
2023-03-19 15:42:04 +0700

    GridDB API wrapper

--- packages/data-server/libs/griddb.cjs
+++ packages/data-server/libs/griddb.cjs
@@ -2,20 +2,26 @@ const griddb = require('griddb-node-api')
 
 const containerName = 'WorldPopulation'
 
-const factory = griddb.StoreFactory.getInstance();
-try {
+const initStore = async () => {
+	const factory = griddb.StoreFactory.getInstance();
+	try {
+		// Connect to GridDB Cluster
+		const store = await factory.getStore({
+			host: "127.0.0.1",
+			// transaction port (see in griddb config gs_cluster.json)
+			port: 10001,
+			clusterName: "myCluster",
+			username: "admin",
+			password: "admin",
+		});
+		return store;
+	} catch (e) {
+		throw e;
+	}
+};
 
-	// Connect to GridDB Cluster
-	const store = factory.getStore({
-		host: "127.0.0.1",
-		// transaction port (see in griddb config gs_cluster.json)
-		port: 10001,
-		clusterName: "myCluster",
-		username: "admin",
-		password: "admin",
-	});
-
-	// Initialize container but not yet create it
+// Initialize container but not yet create it
+function initContainer() {
 	const conInfo = new griddb.ContainerInfo({
 		'name': containerName,
 		'columnInfoList': [
@@ -25,82 +31,122 @@ try {
 		'type': griddb.ContainerType.TIME_SERIES
 	});
 
-	let timeSeriesDB;
-	// Create container
-	store.putContainer(conInfo).then((db) => {
-		timeSeriesDB = db
+	return conInfo
+}
 
-		for (var index = 0; index < store.partitionController.partitionCount; index++) {
-			store.partitionController.getContainerNames(index, 0, -1)
-				.then(nameList => {
-					nameList.forEach(element => {
-						// Get container information
-						store.getContainerInfo(element)
-							.then((info) => {
-								console.log("Get ContainerInfo: \n    name=%s", info.name);
-								if (info.type == griddb.ContainerType.COLLECTION) {
-									console.log('    type=Collection');
-								} else {
-									console.log('    type=TimeSeries');
-								}
-								console.log("    rowKeyAssigned=%s", info.rowKey.toString());
-								console.log("    columnCount=%d", info.columnInfoList.length);
-								info.columnInfoList.forEach(
-									element => console.log("    column (%s, %d)", element[0], element[1])
-								);
-								console.log('Success!');
-							})
-						console.log(element)
-					});
-					return true;
-				})
-				.catch(err => {
-					if (err.constructor.name == 'GSException') {
-						for (var i = 0; i < err.getErrorStackSize(); i++) {
-							console.log("[%d]", i);
-							console.log(err.getErrorCode(i));
-							console.log(err.getMessage(i));
-						}
-					} else {
-						console.log(err);
+async function createContainer(store, conInfo) {
+	try {
+		const timeSeriesDB = await store.putContainer(conInfo);
+		return timeSeriesDB;
+	} catch (err) {
+		console.error(err);
+		throw err;
+	}
+}
+
+async function initGridDbTS() {
+	try {
+		const store = await initStore()
+		const conInfo = await initContainer()
+		const timeSeriesDb = await createContainer(store, conInfo)
+		return timeSeriesDb
+	}
+	catch (e) {
+		console.error(err);
+		throw err;
+	}
+}
+
+async function containersInfo(store) {
+	for (var index = 0; index < store.partitionController.partitionCount; index++) {
+		store.partitionController.getContainerNames(index, 0, -1)
+			.then(nameList => {
+				nameList.forEach(element => {
+					// Get container information
+					store.getContainerInfo(element)
+						.then((info) => {
+							console.log("Get ContainerInfo: \n    name=%s", info.name);
+							if (info.type == griddb.ContainerType.COLLECTION) {
+								console.log('    type=Collection');
+							} else {
+								console.log('    type=TimeSeries');
+							}
+							console.log("    rowKeyAssigned=%s", info.rowKey.toString());
+							console.log("    columnCount=%d", info.columnInfoList.length);
+							info.columnInfoList.forEach(
+								element => console.log("    column (%s, %d)", element[0], element[1])
+							);
+							console.log('Success!');
+						})
+					console.log(element)
+				});
+				return true;
+			})
+			.catch(err => {
+				if (err.constructor.name == 'GSException') {
+					for (var i = 0; i < err.getErrorStackSize(); i++) {
+						console.log("[%d]", i);
+						console.log(err.getErrorCode(i));
+						console.log(err.getMessage(i));
 					}
-				});
-		}
-
-		db.put([getTimestamp(), 8022704491]).then(() => {
-			console.log('Insert Data Ok')
-			const query = timeSeriesDB.query("select *");
-			query.fetch().then((rowset) => {
-				while (rowset.hasNext()) {
-					var row = rowset.next();
-					console.log("Date =", row[0], "Value =", row[1].toString());
+				} else {
+					console.log(err);
 				}
-			}).catch(err => console.log(err))
+			});
+	}
+}
 
-		}).catch(err => console.log(err))
+async function insert(data, db) {
+	try {
+		await db.put(data);
+		return { ok: true };
+	} catch (err) {
+		console.log(err);
+		return { ok: false, error: err };
+	}
+}
 
-	}).catch(err => console.error(err))
+// Query all data
+async function queryAll(db) {
+	const query = db.query("select *");
 
-	/** Delete container
-	store.dropContainer(containerName).then(() => {
+	try {
+		const rowset = await query.fetch();
+		const results = [];
 
-		// get container names
-		for (var index = 0; index < store.partitionController.partitionCount; index++) {
-			store.partitionController.getContainerNames(index, 0, -1)
-				.then(nameList => {
-					nameList.forEach(element => console.log(element));
-					return true;
-				});
+		while (rowset.hasNext()) {
+			const row = rowset.next();
+			const rowData = { date: row[0], value: row[1] };
+			results.push(rowData);
 		}
 
-	})
-	*/
+		return results;
+	} catch (err) {
+		console.log(err);
+		throw err;
+	}
+}
 
-} catch (error) {
-	console.log(error)
+// Delete container
+async function dropContainer(store, containerName) {
+	store.dropContainer(containerName).then(() => { return "OK" }).catch(e => { throw new Error(e) })
 }
 
 function getTimestamp() {
 	return (new Date()).getTime()
 }
+
+export {
+	initStore,
+	initContainer,
+	initGridDbTS,
+	createContainer,
+	insert as insertData,
+	queryAll as queryAllData,
+	dropContainer,
+	containersInfo
+}
+
+
+
 
