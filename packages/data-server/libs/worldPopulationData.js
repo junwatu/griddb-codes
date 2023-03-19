dd17cbefb5da191b7a3b7b85aceb4d105ecf7736
Equan P <equan.p@gmail.com>
2023-03-19 11:45:17 +0700

    better error handling

--- packages/data-server/libs/worldPopulationData.js
+++ packages/data-server/libs/worldPopulationData.js
@@ -4,62 +4,75 @@ const worldPopDataSource = "https://www.worldometers.info/world-population/";
 const worldPopDataByCountrySource =
   "https://www.worldometers.info/world-population/population-by-country/";
 
+
 /**
  * fetch world population data
  */
 const fetchWorldPopulationData = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  let browser
+  try {
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  await page.goto(worldPopDataSource);
+    await page.goto(worldPopDataSource, { timeout: 60000 });
 
-  const extractData = async () => {
-    // Extract data from the page
-    const data = await page.evaluate(() => {
-      const populationElement = document.querySelector(".rts-counter");
-      const population = populationElement.textContent.replace(/[\n\s]+/g, "");
-      return { population };
-    });
+    const extractData = async () => {
+      // Extract data from the page
+      const data = await page.evaluate(() => {
+        const populationElement = document.querySelector(".rts-counter");
+        const population = populationElement.textContent.replace(/[\n\s]+/g, "");
+        return { population };
+      });
 
-    return data;
-  };
+      return data;
+    };
 
-  const popData = await extractData();
-  await browser.close();
-
-  return popData;
+    const popData = await extractData();
+    return popData;
+  } catch (error) {
+    console.error('Error fetching world population data:', error);
+    throw new Error(error)
+  } finally {
+    await browser.close();
+  }
 };
 
 /**
  * fetch world population data by country
  */
 const fetchCountryPopulationData = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  let browser
+  try {
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  await page.goto(worldPopDataByCountrySource);
+    await page.goto(worldPopDataByCountrySource, { timeout: 60000 });
 
-  // Extract data from the page
-  const data = await page.evaluate(() => {
-    const tableRows = document.querySelectorAll("#example2 tbody tr");
-    const countryPopulationData = [];
+    const data = await page.evaluate(() => {
+      const tableRows = document.querySelectorAll("#example2 tbody tr");
+      const countryPopulationData = [];
 
-    tableRows.forEach((row) => {
-      const countryElement = row.querySelector("td:nth-child(2) a");
-      const country = countryElement.textContent.trim();
+      tableRows.forEach((row) => {
+        const countryElement = row.querySelector("td:nth-child(2) a");
+        const country = countryElement.textContent.trim();
 
-      const populationElement = row.querySelector("td:nth-child(3)");
-      const population = populationElement.textContent.trim();
+        const populationElement = row.querySelector("td:nth-child(3)");
+        const population = populationElement.textContent.trim();
 
-      countryPopulationData.push({ country, population });
+        countryPopulationData.push({ country, population });
+      });
+
+      return countryPopulationData;
     });
 
-    return countryPopulationData;
-  });
+    return data;
 
-  await browser.close();
-
-  return data;
+  } catch (error) {
+    console.error('Error fetching country population data:', error);
+    throw new Error(error)
+  } finally {
+    await browser.close();
+  }
 };
 
 export { fetchCountryPopulationData, fetchWorldPopulationData };
