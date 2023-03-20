8899b2ee236fa6d2b23abd25d521f876594bb60f
Equan P <equan.p@gmail.com>
2023-03-20 12:49:04 +0700

    ws in browser is native ;)

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
-import WebSocket from "ws"
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css'
